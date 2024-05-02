2a3f455a5f8b53a1e4a523dbd9f87d01de143561
Mgokayay <aymustafagokay@gmail.com>
2024-05-02 16:42:29 +0300

    medium  method created

--- src/main/java/com/example/demo/entity/MediumCourseGpa.java
+++ src/main/java/com/example/demo/entity/MediumCourseGpa.java
@@ -1,8 +1,11 @@
 package com.example.demo.entity;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MediumCourseGpa implements CourseGpa{
     @Override
     public int getGpa() {
-        return 0;
+        return 5;
     }
 }
