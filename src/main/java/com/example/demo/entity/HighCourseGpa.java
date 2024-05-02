c850e959115dfd5c9b79305013deb3f07285fc31
Mgokayay <aymustafagokay@gmail.com>
2024-05-02 16:42:41 +0300

    high  method created

--- src/main/java/com/example/demo/entity/HighCourseGpa.java
+++ src/main/java/com/example/demo/entity/HighCourseGpa.java
@@ -1,8 +1,11 @@
 package com.example.demo.entity;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class HighCourseGpa implements CourseGpa{
     @Override
     public int getGpa() {
-        return 0;
+        return 10;
     }
 }
