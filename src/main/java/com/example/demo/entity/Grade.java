4a3313325f9b19f6073977dc69f7f83bb3bcaa29
Mgokayay <aymustafagokay@gmail.com>
2024-05-02 16:41:14 +0300

    grade entity created

---
+++ src/main/java/com/example/demo/entity/Grade.java
@@ -0,0 +1,12 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Grade {
+
+    private Integer coefficient;
+    private String note;
+}
