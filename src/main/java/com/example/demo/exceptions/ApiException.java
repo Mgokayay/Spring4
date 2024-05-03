e9e8ecd8d7533e3dc5f9c0a25f644389c5f17a35
Mgokayay <aymustafagokay@gmail.com>
2024-05-03 18:28:55 +0300

    api exception created

---
+++ src/main/java/com/example/demo/exceptions/ApiException.java
@@ -0,0 +1,15 @@
+package com.example.demo.exceptions;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+public class ApiException extends RuntimeException{
+
+    private HttpStatus httpStatus;
+
+    public ApiException(String message,HttpStatus httpStatus){
+        super(message);
+        this.httpStatus=httpStatus;
+    }
+}
